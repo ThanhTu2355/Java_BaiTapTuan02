2e58d20f15004bd8ed36fbaff987e90f85c21664
ADMIN <ADMIN@PC331>
2024-08-14 10:53:47 +0700

    bai 2

---
+++ src/main/java/BaiTap01/ChuongTrinh.java
@@ -0,0 +1,29 @@
+/*
+ */
+package BaiTap01;
+
+public class ChuongTrinh {
+
+    public static void main(String[] args) {
+        TaiKhoan tk1;
+        TaiKhoan tk2;
+
+        tk1 = new TaiKhoan("001", "Chuno", 30000000);
+        tk2 = new TaiKhoan("002", "Conno", 5000000);
+
+        System.out.println("THONG TIN TAI KHOAN");
+        System.out.println("id: " + tk1.getId() + " name: " + tk1.getName() + " So du: " + tk1.getSoDu());
+        System.out.println("id: " + tk2.getId() + " name: " + tk2.getName() + " So du: " + tk2.getSoDu());
+
+        tk1.napTien(70000000);
+        tk2.napTien(5000000);
+        System.out.println("SAU KHI RUT-NAP TIEN");
+        System.out.println("id: " + tk1.getId() + " name: " + tk1.getName() + " So du: " + tk1.getSoDu());
+        System.out.println("id: " + tk2.getId() + " name: " + tk2.getName() + " So du: " + tk2.getSoDu());
+
+        tk2.chuyenTien(tk1, 10000000);
+        System.out.println("TAI KHOAN SAU KHI CHUYEN KHOAN");
+        System.out.println("id: " + tk1.getId() + " name: " + tk1.getName() + " So du: " + tk1.getSoDu());
+        System.out.println("id: " + tk2.getId() + " name: " + tk2.getName() + " So du: " + tk2.getSoDu());
+    }
+}
---
+++ src/main/java/BaiTap01/TaiKhoan.java
@@ -0,0 +1,52 @@
+/*
+ */
+package BaiTap01;
+
+public class TaiKhoan {
+
+    private String id;
+    private String name;
+    private int soDu;
+
+    public TaiKhoan() {
+    }
+
+    public TaiKhoan(String id, String name, int soDu) {
+        this.id = id;
+        this.name = name;
+        this.soDu = soDu;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSoDu() {
+        return soDu;
+    }
+
+    public void napTien(int amount) {
+        soDu += amount;
+    }
+
+    public void rutTien(int amount) {
+        if (soDu >= amount) {
+            soDu -= amount;
+        } else {
+            System.out.println("Giao dich khong thanh cong!");
+        }
+    }
+
+    public void chuyenTien(TaiKhoan other, int amount) {
+        if (soDu >= amount) {
+            soDu -= amount;
+            other.soDu += amount;
+        } else {
+            System.out.println("Giao dich khong thanh cong!");
+        }
+    }
+}
