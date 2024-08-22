29029ccbef4146d3f76e20f881d9f48d3c6fbd5e
nguyentu1112005 <nguyentu1112005@gmail.com>
2024-08-22 10:15:35 +0700

    code phan sap sep lab02.baitap02

--- src/main/java/lab02_BaiTap02/ChuongTrinh.java
+++ src/main/java/lab02_BaiTap02/ChuongTrinh.java
@@ -3,7 +3,9 @@
 package lab02_BaiTap02;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class ChuongTrinh {
@@ -91,5 +93,14 @@ public class ChuongTrinh {
                 System.out.println("--------------------");
             }
         }
+    }
+    public static void sapXep(ArrayList<SinhVien> dssv) {
+        Comparator<SinhVien> cmp = new Comparator<SinhVien>() {
+            @Override
+            public int compare(SinhVien sv1, SinhVien sv2) {
+                return Double.valueOf(sv1.getDiem()).compareTo(Double.valueOf(sv2.getDiem()));
+            }
+        };
+        Collections.sort(dssv,cmp);
     }
 }
